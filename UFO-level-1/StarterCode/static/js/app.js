7169211570a8a9b64dbf11d43fec3f6e83b395ea
Emayer <53119577+jeneric1023@users.noreply.github.com>
2019-12-09 09:17:21 -0600

    code to upload data to webpage

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -2,3 +2,22 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the weather data from data.js
+console.log(data);
+
+// Step 5: Use d3 to update each cell's text with
+// weather report values (weekday, date, high, low)
+data.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
