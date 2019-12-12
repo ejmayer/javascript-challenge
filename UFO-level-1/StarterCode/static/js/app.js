31ad609aab159f9ffbf8c8648a1a48c70e63263d
Emayer <53119577+jeneric1023@users.noreply.github.com>
2019-12-11 18:22:40 -0600

    worked on search code

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,23 +1,63 @@
 // from data.js
-var tableData = data;
+var ufoData = data;
 
-// YOUR CODE HERE!
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the weather data from data.js
-console.log(data);
+// Console.log the ufo data from data.js
+// console.log(data);
 
-// Step 5: Use d3 to update each cell's text with
-// weather report values (weekday, date, high, low)
+// Use d3 to update each cell's text with ufoReport values (date/time, city, state, country, shape, duration, comments)
 data.forEach(function(ufoReport) {
-    console.log(ufoReport);
+    // console.log(ufoReport);
     var row = tbody.append("tr");
     Object.entries(ufoReport).forEach(function([key, value]) {
-      console.log(key, value);
-      // Append a cell to the row for each value
-      // in the weather report object
+      // console.log(key, value);
+      // Append a cell to the row for each value in ufoReport
       var cell = row.append("td");
       cell.text(value);
+
     });
   });
+
+function handleClick() {
+
+// create search function
+// get date from search filter location
+
+  var searchDate = d3.select("#datetime").property("value");
+  var searchDateData = ufoData;
+  
+  console.log(searchDateData);
+  console.log(searchDate);
+
+  tbody.html("");
+
+  // filter table data to match search date value
+  if (searchDate) {
+    searchDateData = searchDateData.filter(row => row.datetime === searchDate);
+
+    data.forEach(function(searchDateData) {
+      // console.log(search date data);
+      var row = tbody.append("tr");
+      Object.entries(searchDateData).forEach(function([key, value]) {
+        // console.log(key, value);
+        // Append a cell to the row for each value in search date
+        var cell = row.append("td");
+        cell.text(value);
+  
+      });
+    });
+  }
+
+// load table with new "searched" data
+  //ufoReport(searchDateData);
+  console.log(searchDateData);
+}
+
+
+// event for search click
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// load table with original data (all dates)
+// loadTable(ufoData);
