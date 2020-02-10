eb681e7276455bd70a835508cacf632b81aa7d64
Emayer <53119577+jeneric1023@users.noreply.github.com>
2020-02-09 23:45:47 -0600

    search function does not load correctly

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -5,20 +5,20 @@ var ufoData = data;
 var tbody = d3.select("tbody");
 
 // Console.log the ufo data from data.js
-// console.log(data);
+console.log(data);
 
 // Use d3 to update each cell's text with ufoReport values (date/time, city, state, country, shape, duration, comments)
 data.forEach(function(ufoReport) {
-    // console.log(ufoReport);
-    var row = tbody.append("tr");
-    Object.entries(ufoReport).forEach(function([key, value]) {
-      // console.log(key, value);
-      // Append a cell to the row for each value in ufoReport
-      var cell = row.append("td");
-      cell.text(value);
+  // console.log(ufoReport);
+  var row = tbody.append("tr");
+  Object.entries(ufoReport).forEach(function([key, value]) {
+    // console.log(key, value);
+    // Append a cell to the row for each value in ufoReport
+    var cell = row.append("td");
+    cell.text(value);
 
-    });
   });
+});
 
 function handleClick() {
 
@@ -38,20 +38,24 @@ function handleClick() {
     searchDateData = searchDateData.filter(row => row.datetime === searchDate);
 
     data.forEach(function(searchDateData) {
-      // console.log(search date data);
+      // console.log(searchDateData);
       var row = tbody.append("tr");
       Object.entries(searchDateData).forEach(function([key, value]) {
         // console.log(key, value);
         // Append a cell to the row for each value in search date
         var cell = row.append("td");
         cell.text(value);
+        
   
+
       });
     });
   }
 
+
+  
 // load table with new "searched" data
-  //ufoReport(searchDateData);
+//  ufoReport(searchDateData);
   console.log(searchDateData);
 }
 
